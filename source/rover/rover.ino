fa44b640fe5472b08dfb3e1c10e31f8c5b18fe5a
telekrex <telekrex@gmail.com>
2022-08-20 15:25:33 -0400

    Update rover.ino

--- source/rover/rover.ino
+++ source/rover/rover.ino
@@ -3,14 +3,23 @@ Rover computer service - turns COM data into action
 */
 
 
+int control = 0;
+int LMotor = 7;
+int RMotor = 8;
+
+
 void setup() {
   Serial.begin(115200);
   Serial.setTimeout(0);
+  pinMode(LMotor, OUTPUT);
+  pinMode(RMotor, OUTPUT);
 
 }
 
 
 void loop() {
-  int incomdata = Serial.read();
+  control = Serial.read();
+  digitalWrite(LMotor, LOW);
+  digitalWrite(RMotor, LOW);
 
 }
